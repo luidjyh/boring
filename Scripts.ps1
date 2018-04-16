206060ec879bb7bd59afff22c6aaf5038131aac3
luidjyh <38428237+luidjyh@users.noreply.github.com>
2018-04-16 16:17:36 +0200

    first commit

---
+++ Scripts.ps1
@@ -0,0 +1,21 @@
+﻿$Variable: = powershell
+
+$variable = "Powershell"
+
+$variable 
+#Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+
+$variable = "Powershell"
+$var1 = 10 
+$var2 = 10
+$var3 = 10
+
+if ($var1 -eq $var2 -and $var2 -eq $var3) { 
+Write-Host "Cha marche"
+} else { 
+Write-Host "Cha marche pas" 
+
+
+$fruits = @(" pomme", "poire", 45, $var1)
+
+$fruits
